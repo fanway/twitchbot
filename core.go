436ace79fa4eeb39a7e5a60ebb9e47dbe5be0d7b
Ivan Avvakumov <avvakumov@protonmail.com>
2019-08-10 16:26:27 +0300

    fix the file descriptor race(when flushing) && correct the name matching

--- core.go
+++ core.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/textproto"
@@ -48,27 +49,24 @@ func (bot *Bot) reader(wg *sync.WaitGroup) {
 	for {
 		line, err := tp.ReadLine()
 		if err != nil {
-			return
+			panic(err)
 		}
-		fmt.Println(line)
-		messages := strings.Split(line, "\r\n")
-		for _, msg := range messages {
-			if strings.Contains(msg, "PRIVMSG") {
-				userdata := strings.Split(msg, ".tmi.twitch.tv PRIVMSG "+bot.Channel)
-				username := strings.Split(userdata[0], "@")[1]
-				usermessage := strings.Replace(userdata[1], " :", "", 1)
-				userMessageCommand := strings.Split(usermessage, " ")[0]
-				if username == "funwayz" && userMessageCommand[0:1] == "!" {
-					fmt.Println("test2")
-					go bot.Commands(userMessageCommand)
-				}
-				fmt.Fprintf(w, "[%s] %s: %s\n", time.Now().Format("2006-01-02 15:04:05 -0700 MST"), username, usermessage)
-			} else if strings.Contains(msg, "PING") {
-				bot.Pong(msg)
-				fmt.Fprintln(w, msg)
-			} else {
-				fmt.Fprintln(w, msg)
+		if strings.Contains(line, "PRIVMSG") {
+			userdata := strings.Split(line, ".tmi.twitch.tv PRIVMSG "+bot.Channel)
+			username := strings.Split(userdata[0], "@")[1]
+			usermessage := strings.Replace(userdata[1], " :", "", 1)
+			userMessageCommand := strings.Split(usermessage, " ")[0]
+			fmt.Fprintf(w, "[%s] %s: %s\n", time.Now().Format("2006-01-02 15:04:05 -0700 MST"), username, usermessage)
+			w.Flush()
+			if username == "funwayz" && userMessageCommand[0:1] == "!" {
+				go bot.Commands(userMessageCommand)
 			}
+		} else if strings.Contains(line, "PING") {
+			bot.Pong(line)
+			fmt.Fprintln(w, line)
+			w.Flush()
+		} else {
+			fmt.Fprintln(w, line)
 			w.Flush()
 		}
 		defer wg.Done()
@@ -79,11 +77,22 @@ func (bot *Bot) Commands(command string) {
 	if strings.Contains(command, "!logs(") {
 		t := strings.Split(command, "(")[1]
 		name := strings.Split(t, ")")[0]
-		fmt.Println(name)
+		currentPosition, _ := bot.File.Seek(0, 1)
+		offset, err := bot.File.Seek(0, io.SeekStart)
+		if err != nil {
+			panic(err)
+		}
 		r := bufio.NewScanner(bot.File)
+		var str string
 		for r.Scan() {
-			if strings.Contains(r.Text(), name) {
-				fmt.Fprintf(bot.Conn, "PRIVMSG %s :%s\r\n", bot.Channel, r.Text())
+			str = r.Text()
+			if strings.Count(str, ":") > 2 {
+				if strings.Contains(strings.Split(str, ":")[2], name) {
+					fmt.Fprintf(bot.Conn, "PRIVMSG %s :%s\r\n", bot.Channel, str)
+				}
+			}
+			if err := r.Err(); err != nil {
+				fmt.Println(err)
 			}
 		}
 	}
