7d693faeb1a730589b45910b389ab0d61ec965d3
Ivan Avvakumov <avvakumov@protonmail.com>
2020-02-20 04:40:46 +0300

    Refactor to microservices && add the find feature

---
+++ bot.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/textproto"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type SmartVote struct {
+	Options map[string]int
+	Votes   map[string]string
+}
+
+type Utils struct {
+	SmartVote SmartVote
+}
+
+type Bot struct {
+	Channel   string
+	Name      string
+	Port      string
+	OAuth     string
+	Server    string
+	Conn      net.Conn
+	File      *os.File
+	Authority map[string]string
+	Status    string
+	Utils     Utils
+}
+
+// connects to twitch chat
+func (bot *Bot) Connect(wg *sync.WaitGroup) {
+	var err error
+	bot.Conn, err = net.Dial("tcp", bot.Server+":"+bot.Port)
+	if err != nil {
+		fmt.Printf("Unable to connect!")
+	}
+	fmt.Printf("connected to %s\n", bot.Channel)
+	fmt.Fprintf(bot.Conn, "PASS %s\r\n", bot.OAuth)
+	fmt.Fprintf(bot.Conn, "NICK %s\r\n", bot.Name)
+	fmt.Fprintf(bot.Conn, "JOIN %s\r\n", bot.Channel)
+	wg.Add(1)
+	go bot.reader(wg)
+}
+
+func (bot *Bot) Pong(line string) {
+	pong := strings.Split(line, "PING")
+	fmt.Fprintf(bot.Conn, "PONG %s\r\n", pong[1])
+}
+
+// reader and parser
+func (bot *Bot) reader(wg *sync.WaitGroup) {
+	tp := textproto.NewReader(bufio.NewReader(bot.Conn))
+	w := bufio.NewWriter(bot.File)
+	pasteFile, err := os.OpenFile("paste.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	pasteWriter := bufio.NewWriter(pasteFile)
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		// parsing chat
+		if strings.Contains(line, "PRIVMSG") {
+			userdata := strings.Split(line, ".tmi.twitch.tv PRIVMSG "+bot.Channel)
+			username := strings.Split(userdata[0], "@")[1]
+			usermessage := strings.Replace(userdata[1], " :", "", 1)
+			fmt.Fprintf(w, "[%s] %s: %s\n", time.Now().Format("2006-01-02 15:04:05 -0700 MST"), username, usermessage)
+			w.Flush()
+			messageLength := len(usermessage)
+			if bot.Status == "smartvote" && messageLength == 1 {
+				if _, ok := bot.Utils.SmartVote.Options[usermessage]; ok {
+					if _, ok := bot.Utils.SmartVote.Votes[username]; !ok {
+						bot.Utils.SmartVote.Options[usermessage]++
+						bot.Utils.SmartVote.Votes[username] = usermessage
+					}
+				}
+			}
+			if messageLength >= 300 && messageLength <= 2000 {
+				fmt.Fprintf(pasteWriter, "%s\n\n", usermessage)
+			}
+
+			if bot.Authority[username] == "top" && usermessage[0:1] == "!" {
+				go bot.Commands(usermessage, username)
+			}
+		} else if strings.Contains(line, "PING") { // response to keep connection alive
+			bot.Pong(line)
+			fmt.Fprintln(w, line)
+			w.Flush()
+		}
+		/*
+			else {
+				fmt.Fprintln(w, line)
+				w.Flush()
+			}
+		*/
+		defer wg.Done()
+	}
+}
+
+// chat commands
+func (bot *Bot) Commands(command string, username string) {
+	var cmd Command
+	var err error
+	err = cmd.Parse(command)
+	switch cmd.Name {
+	// !logs(username, timeStart, timeEnd)
+	case "logs":
+		err = bot.LogsCommand(cmd.Params)
+	// !smartvote(lowerBound, upperBound)
+	case "smartvote":
+		err = bot.SmartVoteCommand(cmd.Params)
+	// !voteoptions
+	case "voteoptions":
+		err = bot.VoteOptionsCommand()
+	// !stopvote
+	case "stopvote":
+		bot.Status = "Running"
+	}
+	if err != nil {
+		bot.Status = "Running"
+		fmt.Println(err)
+	}
+}
+
+func authorityInit() map[string]string {
+	file, err := ioutil.ReadFile("authority.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var m map[string]string
+	if err = json.Unmarshal(file, &m); err != nil {
+		fmt.Println(err)
+	}
+	return m
+}
+
+func StartBot(channel string) {
+	wg := new(sync.WaitGroup)
+	logfile, err := os.OpenFile(channel+".log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logfile.Close()
+	m := authorityInit()
+	bot := Bot{
+		Channel:   channel,
+		Name:      "funwayz",
+		Port:      "6667",
+		OAuth:     os.Getenv("TWITCH_OAUTH_ENV"),
+		Server:    "irc.twitch.tv",
+		File:      logfile,
+		Conn:      nil,
+		Authority: m,
+	}
+	bot.Connect(wg)
+	wg.Wait()
+}
--- core.go
+++ core.go
@@ -1,168 +1,286 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net"
-	"net/textproto"
-	"os"
-	"strings"
-	"sync"
-	"time"
+    "database/sql"
+    "fmt"
+    _ "github.com/mattn/go-sqlite3"
+    "net/http"
+    "os"
+    "encoding/json"
+    "strconv"
+    "strings"
+    "bufio"
+    //"io/ioutil"
 )
 
-type SmartVote struct {
-	Options map[string]int
-	Votes   map[string]string
+type IdData struct {
+	Data []struct {
+		ID              string `json:"id"`
+		Login           string `json:"login"`
+		DisplayName     string `json:"display_name"`
+		Type            string `json:"type"`
+		BroadcasterType string `json:"broadcaster_type"`
+		Description     string `json:"description"`
+		ProfileImageURL string `json:"profile_image_url"`
+		OfflineImageURL string `json:"offline_image_url"`
+		ViewCount       int    `json:"view_count"`
+	} `json:"data"`
 }
 
-type Utils struct {
-	SmartVote SmartVote
+type Followers struct {
+    Total int `json:"total"`
+    Data  []struct {
+        FromID     string    `json:"from_id"`
+        FromName   string    `json:"from_name"`
+        ToID       string    `json:"to_id"`
+        ToName     string    `json:"to_name"`
+        FollowedAt string    `json:"followed_at"`
+    } `json:"data"`
+    Pagination struct {
+        Cursor string `json:"cursor"`
+    } `json:"pagination"`
 }
 
-type Bot struct {
-	Channel   string
-	Name      string
-	Port      string
-	OAuth     string
-	Server    string
-	Conn      net.Conn
-	File      *os.File
-	Authority map[string]string
-	Status    string
-	Utils     Utils
+type ChatData struct {
+    Links struct {
+    } `json:"_links"`
+    ChatterCount int `json:"chatter_count"`
+    Chatters     struct {
+        Broadcaster []string      `json:"broadcaster"`
+        Vips        []string `json:"vips"`
+        Moderators  []string      `json:"moderators"`
+        Staff       []string `json:"staff"`
+        Admins      []string  `json:"admins"`
+        GlobalMods  []string `json:"global_mods"`
+        Viewers     []string      `json:"viewers"`
+    } `json:"chatters"`
 }
 
-// connects to twitch chat
-func (bot *Bot) Connect(wg *sync.WaitGroup) {
-	var err error
-	bot.Conn, err = net.Dial("tcp", bot.Server+":"+bot.Port)
-	if err != nil {
-		fmt.Printf("Unable to connect!")
-	}
-	fmt.Fprintf(bot.Conn, "PASS %s\r\n", bot.OAuth)
-	fmt.Fprintf(bot.Conn, "NICK %s\r\n", bot.Name)
-	fmt.Fprintf(bot.Conn, "JOIN %s\r\n", bot.Channel)
-	wg.Add(1)
-	go bot.reader(wg)
-}
-
-func (bot *Bot) Pong(line string) {
-	pong := strings.Split(line, "PING")
-	fmt.Fprintf(bot.Conn, "PONG %s\r\n", pong[1])
-}
-
-// reader and parser
-func (bot *Bot) reader(wg *sync.WaitGroup) {
-	tp := textproto.NewReader(bufio.NewReader(bot.Conn))
-	w := bufio.NewWriter(bot.File)
-	pasteFile, err := os.OpenFile("paste.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
-	pasteWriter := bufio.NewWriter(pasteFile)
-	for {
-		line, err := tp.ReadLine()
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		// parsing chat
-		if strings.Contains(line, "PRIVMSG") {
-			userdata := strings.Split(line, ".tmi.twitch.tv PRIVMSG "+bot.Channel)
-			username := strings.Split(userdata[0], "@")[1]
-			usermessage := strings.Replace(userdata[1], " :", "", 1)
-			fmt.Fprintf(w, "[%s] %s: %s\n", time.Now().Format("2006-01-02 15:04:05 -0700 MST"), username, usermessage)
-			w.Flush()
-			messageLength := len(usermessage)
-			if bot.Status == "smartvote" && messageLength == 1 {
-				if _, ok := bot.Utils.SmartVote.Options[usermessage]; ok {
-					if _, ok := bot.Utils.SmartVote.Votes[username]; !ok {
-						bot.Utils.SmartVote.Options[usermessage]++
-						bot.Utils.SmartVote.Votes[username] = usermessage
-					}
-				}
-			}
-			if messageLength >= 200 && messageLength <= 500 {
-				fmt.Fprintf(pasteWriter, "%s\n\n", usermessage)
-			}
-
-			if bot.Authority[username] == "top" && usermessage[0:1] == "!" {
-				go bot.Commands(usermessage, username)
-			}
-		} else if strings.Contains(line, "PING") { // response to keep connection alive
-			bot.Pong(line)
-			fmt.Fprintln(w, line)
-			w.Flush()
-		} else {
-			fmt.Fprintln(w, line)
-			w.Flush()
-		}
-		defer wg.Done()
-	}
-}
-
-// chat commands
-func (bot *Bot) Commands(command string, username string) {
-	var cmd Command
-	var err error
-	cmd.Parse(command)
-	switch cmd.Name {
-	// !logs(username, timeStart, timeEnd)
-	case "logs":
-		err = bot.LogsCommand(cmd.Params)
-	// !smartvote(lowerBound, upperBound)
-	case "smartvote":
-		err = bot.SmartVoteCommand(cmd.Params)
-	// !voteoptions
-	case "voteoptions":
-		err = bot.VoteOptionsCommand()
-	// !stopvote
-	case "stopvote":
-		bot.Status = "Running"
-	}
-	if err != nil {
-		bot.Status = "Running"
-		fmt.Println(err)
-	}
-}
-
-func authorityInit() map[string]string {
-	file, err := ioutil.ReadFile("authority.txt")
+func findPerson(name string) {
+    db, err := sql.Open("sqlite3", "./data.db?_busy_timeout=5000&cache=shared&mode=rwc")
+    if err != nil {
+        panic(err)
+    }
+    db.SetMaxOpenConns(1)
+    defer db.Close()
+    tx, err := db.Begin()
+    if err != nil {
+        fmt.Println(err)
+        return 
+    }
+    defer tx.Rollback()
+	client := &http.Client{}
+	var id string
+    url := "https://api.twitch.tv/helix/users?login=" + name
+	req, _ := http.NewRequest("GET", url, nil)
+    req.Header.Set("Authorization", "Bearer " + strings.Split(os.Getenv("TWITCH_OAUTH_ENV"), ":")[1])
+    res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	var m map[string]string
-	if err = json.Unmarshal(file, &m); err != nil {
+	decoder := json.NewDecoder(res.Body)
+	var iddata IdData 
+    err = decoder.Decode(&iddata)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    id = iddata.Data[0].ID
+    url = "https://api.twitch.tv/helix/users/follows?first=100&from_id=" + id 
+    req, _ = http.NewRequest("GET", url, nil)
+    req.Header.Set("Client-ID", os.Getenv("TWITCH_CLIENT_ID"))
+
+	res, err = client.Do(req)
+
+	if err != nil {
 		fmt.Println(err)
+	    return	
 	}
-	return m
+	decoder = json.NewDecoder(res.Body)
+	var followers Followers
+    err = decoder.Decode(&followers)
+    if err != nil {
+        fmt.Println(err)
+        return 
+    }
+
+    cursor := followers.Pagination.Cursor
+    for i:=0; i < followers.Total / 100; i++ {
+        url = "https://api.twitch.tv/helix/users/follows?first=100&from_id=" + id + "&after=" + cursor 
+        req, _ = http.NewRequest("GET", url, nil)
+        req.Header.Set("Client-ID", os.Getenv("TWITCH_CLIENT_ID"))
+
+        res, err = client.Do(req)
+
+        if err != nil {
+            fmt.Println(err)
+            return	
+        }
+        decoder = json.NewDecoder(res.Body)
+        var temp Followers
+        err = decoder.Decode(&temp)
+        if err != nil {
+            fmt.Println(err)
+            return 
+        }
+        followers.Data = append(followers.Data, temp.Data...)
+        cursor = temp.Pagination.Cursor
+    }
+
+    _, err = tx.Exec("CREATE TEMPORARY TABLE Follow(Id INTEGER PRIMARY KEY, FromId TEXT, FromName TEXT, ToId TEXT, ToName TEXT, FollowedAt TEXT);")
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    for _, d := range followers.Data {
+        _, err := tx.Exec("INSERT INTO temp.Follow(FromId, FromName, ToId, ToName, FollowedAt) values($1, $2, $3, $4, $5);", d.FromID, d.FromName, d.ToID, d.ToName, d.FollowedAt)
+        if err != nil {
+            fmt.Println("temp table")
+            fmt.Println(err)
+        }
+    }
+
+    rows, err := tx.Query("SELECT * FROM temp.Follow tm WHERE NOT EXISTS (SELECT * FROM Followers t WHERE tm.FromId = t.FromId AND tm.ToId = t.Toid);")
+
+    defer rows.Close()
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println("New channels: ")
+    for rows.Next() {
+        var idx int 
+        var fromId string
+        var fromName string
+        var toId string
+        var toName string
+        var followersAt string
+        err = rows.Scan(&idx, &fromId, &fromName, &toId, &toName, &followersAt)
+        if err != nil {
+            fmt.Println(err)
+        }
+        fmt.Print(toName + " ")
+        _, err = tx.Exec("INSERT INTO Followers(FromId, FromName, ToId, ToName, FollowedAt) values($1, $2, $3, $4, $5);", fromId, fromName, toId, toName, followersAt)
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+    fmt.Println("")
+
+    rows, err = tx.Query("SELECT t.Id, t.ToName FROM Followers t WHERE t.FromId=$1 AND NOT EXISTS (SELECT * FROM temp.Follow tm WHERE tm.ToId=t.ToId);", id) 
+
+    defer rows.Close()
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println("Unfollowed channels: ")
+    for rows.Next() {
+        var idx int
+        var toName string
+        err := rows.Scan(&idx, &toName)
+        if err != nil {
+            fmt.Println(err)
+        }
+        fmt.Print(toName + " ")
+        _, err = tx.Exec("DELETE FROM Followers WHERE id=$1", strconv.Itoa(idx))
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+    fmt.Println("")
+
+    rows, err = tx.Query("SELECT ToName FROM Followers WHERE FromId=$1;", id) 
+
+    defer rows.Close()
+    if err != nil {
+        fmt.Println(err)
+    } 
+
+    fmt.Println("Currently watching: ")
+    for rows.Next() {
+        var toName string
+        rows.Scan(&toName)
+        url = "https://tmi.twitch.tv/group/user/" + strings.ToLower(toName) + "/chatters"
+
+        req, _ := http.NewRequest("GET", url, nil)
+        res, err := client.Do(req)
+
+        if err != nil {
+            fmt.Println(err)
+            continue 
+        }
+
+        decoder := json.NewDecoder(res.Body)
+
+        var chatData ChatData
+        err = decoder.Decode(&chatData)
+
+        if err != nil {
+            //fmt.Println(toName)
+            //fmt.Println(err)
+            continue 
+        }
+
+        for _, cname := range chatData.Chatters.Vips {
+           if cname == name {
+               fmt.Print(toName + " ")
+           }
+        }
+
+        for _, cname := range chatData.Chatters.Moderators {
+           if cname == name {
+               fmt.Print(toName + " ")
+           }
+        }
+
+        for _, cname := range chatData.Chatters.Viewers {
+           if cname == name {
+               fmt.Print(toName + " ")
+           }
+        }
+    }
+    fmt.Println("")
+
+    tx.Commit()
+    fmt.Println("-----------------------")
+}
+
+func parseCommand(str string) {
+    args := strings.Split(str, " ")
+    if len(args) < 2 {
+        fmt.Println("Not enough args")
+    }
+
+    fmt.Println(len(args))
+    command := args[0]
+    args = args[1:]
+
+    switch command {
+    case "connect":
+        if len(args) != 1 {
+            fmt.Println("Provide channel name")
+            break
+        }
+        go StartBot(args[0])
+    case "find":
+        if len(args) != 1 {
+            fmt.Println("Provide channel name")
+            break
+        }
+
+        findPerson(args[0])
+    }
 }
 
 func main() {
-	wg := new(sync.WaitGroup)
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	channel := scanner.Text()
-	logfile, err := os.OpenFile(channel+".log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logfile.Close()
-	m := authorityInit()
-	bot := Bot{
-		Channel:   channel,
-		Name:      "funwayz",
-		Port:      "6667",
-		OAuth:     os.Getenv("TWITCH_OAUTH_ENV"),
-		Server:    "irc.twitch.tv",
-		File:      logfile,
-		Conn:      nil,
-		Authority: m,
-	}
-	bot.Connect(wg)
-	wg.Wait()
+    scanner := bufio.NewScanner(os.Stdin)
+    for {
+        scanner.Scan()
+        args := scanner.Text()
+        parseCommand(args)
+    }
 }
