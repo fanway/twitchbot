1b84e9e15c7ac7606d5aff6f010ad63f6afafbca
Ivan Avvakumov <avvakumov@protonmail.com>
2020-02-20 09:17:02 +0300

    Fix indentation && drop excess http request

--- core.go
+++ core.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/mattn/go-sqlite3"
-    "net/http"
-    "os"
-    "encoding/json"
-    "strconv"
-    "strings"
-    "bufio"
-    //"io/ioutil"
+	"bufio"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+	//"io/ioutil"
 )
 
 type IdData struct {
@@ -28,259 +29,262 @@ type IdData struct {
 }
 
 type Followers struct {
-    Total int `json:"total"`
-    Data  []struct {
-        FromID     string    `json:"from_id"`
-        FromName   string    `json:"from_name"`
-        ToID       string    `json:"to_id"`
-        ToName     string    `json:"to_name"`
-        FollowedAt string    `json:"followed_at"`
-    } `json:"data"`
-    Pagination struct {
-        Cursor string `json:"cursor"`
-    } `json:"pagination"`
+	Total int `json:"total"`
+	Data  []struct {
+		FromID     string `json:"from_id"`
+		FromName   string `json:"from_name"`
+		ToID       string `json:"to_id"`
+		ToName     string `json:"to_name"`
+		FollowedAt string `json:"followed_at"`
+	} `json:"data"`
+	Pagination struct {
+		Cursor string `json:"cursor"`
+	} `json:"pagination"`
 }
 
 type ChatData struct {
-    Links struct {
-    } `json:"_links"`
-    ChatterCount int `json:"chatter_count"`
-    Chatters     struct {
-        Broadcaster []string      `json:"broadcaster"`
-        Vips        []string `json:"vips"`
-        Moderators  []string      `json:"moderators"`
-        Staff       []string `json:"staff"`
-        Admins      []string  `json:"admins"`
-        GlobalMods  []string `json:"global_mods"`
-        Viewers     []string      `json:"viewers"`
-    } `json:"chatters"`
+	Links struct {
+	} `json:"_links"`
+	ChatterCount int `json:"chatter_count"`
+	Chatters     struct {
+		Broadcaster []string `json:"broadcaster"`
+		Vips        []string `json:"vips"`
+		Moderators  []string `json:"moderators"`
+		Staff       []string `json:"staff"`
+		Admins      []string `json:"admins"`
+		GlobalMods  []string `json:"global_mods"`
+		Viewers     []string `json:"viewers"`
+	} `json:"chatters"`
 }
 
 func findPerson(name string) {
-    db, err := sql.Open("sqlite3", "./data.db?_busy_timeout=5000&cache=shared&mode=rwc")
-    if err != nil {
-        panic(err)
-    }
-    db.SetMaxOpenConns(1)
-    defer db.Close()
-    tx, err := db.Begin()
-    if err != nil {
-        fmt.Println(err)
-        return 
-    }
-    defer tx.Rollback()
+	db, err := sql.Open("sqlite3", "./data.db?_busy_timeout=5000&cache=shared&mode=rwc")
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(1)
+	defer db.Close()
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tx.Rollback()
 	client := &http.Client{}
 	var id string
-    url := "https://api.twitch.tv/helix/users?login=" + name
+	row := tx.QueryRow("SELECT FromId FROM Followers WHERE FromName=$1;", name)
+	err = row.Scan(&id)
+	if err != nil {
+		url := "https://api.twitch.tv/helix/users?login=" + name
+		req, _ := http.NewRequest("GET", url, nil)
+		req.Header.Set("Authorization", "Bearer "+strings.Split(os.Getenv("TWITCH_OAUTH_ENV"), ":")[1])
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		decoder := json.NewDecoder(res.Body)
+		var iddata IdData
+		err = decoder.Decode(&iddata)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		id = iddata.Data[0].ID
+	}
+	url := "https://api.twitch.tv/helix/users/follows?first=100&from_id=" + id
 	req, _ := http.NewRequest("GET", url, nil)
-    req.Header.Set("Authorization", "Bearer " + strings.Split(os.Getenv("TWITCH_OAUTH_ENV"), ":")[1])
-    res, err := client.Do(req)
+	req.Header.Set("Client-ID", os.Getenv("TWITCH_CLIENT_ID"))
+
+	res, err := client.Do(req)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	decoder := json.NewDecoder(res.Body)
-	var iddata IdData 
-    err = decoder.Decode(&iddata)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    id = iddata.Data[0].ID
-    url = "https://api.twitch.tv/helix/users/follows?first=100&from_id=" + id 
-    req, _ = http.NewRequest("GET", url, nil)
-    req.Header.Set("Client-ID", os.Getenv("TWITCH_CLIENT_ID"))
+	var followers Followers
+	err = decoder.Decode(&followers)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	res, err = client.Do(req)
+	cursor := followers.Pagination.Cursor
+	for i := 0; i < followers.Total/100; i++ {
+		url = "https://api.twitch.tv/helix/users/follows?first=100&from_id=" + id + "&after=" + cursor
+		req, _ = http.NewRequest("GET", url, nil)
+		req.Header.Set("Client-ID", os.Getenv("TWITCH_CLIENT_ID"))
+
+		res, err = client.Do(req)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		decoder = json.NewDecoder(res.Body)
+		var temp Followers
+		err = decoder.Decode(&temp)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		followers.Data = append(followers.Data, temp.Data...)
+		cursor = temp.Pagination.Cursor
+	}
+
+	_, err = tx.Exec("CREATE TEMPORARY TABLE Follow(Id INTEGER PRIMARY KEY, FromId TEXT, FromName TEXT, ToId TEXT, ToName TEXT, FollowedAt TEXT);")
 
 	if err != nil {
 		fmt.Println(err)
-	    return	
 	}
-	decoder = json.NewDecoder(res.Body)
-	var followers Followers
-    err = decoder.Decode(&followers)
-    if err != nil {
-        fmt.Println(err)
-        return 
-    }
 
-    cursor := followers.Pagination.Cursor
-    for i:=0; i < followers.Total / 100; i++ {
-        url = "https://api.twitch.tv/helix/users/follows?first=100&from_id=" + id + "&after=" + cursor 
-        req, _ = http.NewRequest("GET", url, nil)
-        req.Header.Set("Client-ID", os.Getenv("TWITCH_CLIENT_ID"))
+	for _, d := range followers.Data {
+		_, err := tx.Exec("INSERT INTO temp.Follow(FromId, FromName, ToId, ToName, FollowedAt) values($1, $2, $3, $4, $5);", d.FromID, d.FromName, d.ToID, d.ToName, d.FollowedAt)
+		if err != nil {
+			fmt.Println("temp table")
+			fmt.Println(err)
+		}
+	}
 
-        res, err = client.Do(req)
+	rows, err := tx.Query("SELECT * FROM temp.Follow tm WHERE NOT EXISTS (SELECT * FROM Followers t WHERE tm.FromId = t.FromId AND tm.ToId = t.Toid);")
 
-        if err != nil {
-            fmt.Println(err)
-            return	
-        }
-        decoder = json.NewDecoder(res.Body)
-        var temp Followers
-        err = decoder.Decode(&temp)
-        if err != nil {
-            fmt.Println(err)
-            return 
-        }
-        followers.Data = append(followers.Data, temp.Data...)
-        cursor = temp.Pagination.Cursor
-    }
+	defer rows.Close()
 
-    _, err = tx.Exec("CREATE TEMPORARY TABLE Follow(Id INTEGER PRIMARY KEY, FromId TEXT, FromName TEXT, ToId TEXT, ToName TEXT, FollowedAt TEXT);")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    if err != nil {
-        fmt.Println(err)
-    }
+	fmt.Println("New channels: ")
+	for rows.Next() {
+		var idx int
+		var fromId string
+		var fromName string
+		var toId string
+		var toName string
+		var followersAt string
+		err = rows.Scan(&idx, &fromId, &fromName, &toId, &toName, &followersAt)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Print(toName + " ")
+		_, err = tx.Exec("INSERT INTO Followers(FromId, FromName, ToId, ToName, FollowedAt) values($1, $2, $3, $4, $5);", fromId, fromName, toId, toName, followersAt)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println("")
 
-    for _, d := range followers.Data {
-        _, err := tx.Exec("INSERT INTO temp.Follow(FromId, FromName, ToId, ToName, FollowedAt) values($1, $2, $3, $4, $5);", d.FromID, d.FromName, d.ToID, d.ToName, d.FollowedAt)
-        if err != nil {
-            fmt.Println("temp table")
-            fmt.Println(err)
-        }
-    }
+	rows, err = tx.Query("SELECT t.Id, t.ToName FROM Followers t WHERE t.FromId=$1 AND NOT EXISTS (SELECT * FROM temp.Follow tm WHERE tm.ToId=t.ToId);", id)
 
-    rows, err := tx.Query("SELECT * FROM temp.Follow tm WHERE NOT EXISTS (SELECT * FROM Followers t WHERE tm.FromId = t.FromId AND tm.ToId = t.Toid);")
+	defer rows.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    defer rows.Close()
+	fmt.Println("Unfollowed channels: ")
+	for rows.Next() {
+		var idx int
+		var toName string
+		err := rows.Scan(&idx, &toName)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Print(toName + " ")
+		_, err = tx.Exec("DELETE FROM Followers WHERE id=$1", strconv.Itoa(idx))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println("")
 
-    if err != nil {
-        fmt.Println(err)
-    }
+	rows, err = tx.Query("SELECT ToName FROM Followers WHERE FromId=$1;", id)
 
-    fmt.Println("New channels: ")
-    for rows.Next() {
-        var idx int 
-        var fromId string
-        var fromName string
-        var toId string
-        var toName string
-        var followersAt string
-        err = rows.Scan(&idx, &fromId, &fromName, &toId, &toName, &followersAt)
-        if err != nil {
-            fmt.Println(err)
-        }
-        fmt.Print(toName + " ")
-        _, err = tx.Exec("INSERT INTO Followers(FromId, FromName, ToId, ToName, FollowedAt) values($1, $2, $3, $4, $5);", fromId, fromName, toId, toName, followersAt)
-        if err != nil {
-            fmt.Println(err)
-        }
-    }
-    fmt.Println("")
+	defer rows.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    rows, err = tx.Query("SELECT t.Id, t.ToName FROM Followers t WHERE t.FromId=$1 AND NOT EXISTS (SELECT * FROM temp.Follow tm WHERE tm.ToId=t.ToId);", id) 
+	fmt.Println("Currently watching: ")
+	for rows.Next() {
+		var toName string
+		rows.Scan(&toName)
+		url = "https://tmi.twitch.tv/group/user/" + strings.ToLower(toName) + "/chatters"
 
-    defer rows.Close()
-    if err != nil {
-        fmt.Println(err)
-    }
+		req, _ := http.NewRequest("GET", url, nil)
+		res, err := client.Do(req)
 
-    fmt.Println("Unfollowed channels: ")
-    for rows.Next() {
-        var idx int
-        var toName string
-        err := rows.Scan(&idx, &toName)
-        if err != nil {
-            fmt.Println(err)
-        }
-        fmt.Print(toName + " ")
-        _, err = tx.Exec("DELETE FROM Followers WHERE id=$1", strconv.Itoa(idx))
-        if err != nil {
-            fmt.Println(err)
-        }
-    }
-    fmt.Println("")
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-    rows, err = tx.Query("SELECT ToName FROM Followers WHERE FromId=$1;", id) 
+		decoder := json.NewDecoder(res.Body)
 
-    defer rows.Close()
-    if err != nil {
-        fmt.Println(err)
-    } 
+		var chatData ChatData
+		err = decoder.Decode(&chatData)
 
-    fmt.Println("Currently watching: ")
-    for rows.Next() {
-        var toName string
-        rows.Scan(&toName)
-        url = "https://tmi.twitch.tv/group/user/" + strings.ToLower(toName) + "/chatters"
+		if err != nil {
+			//fmt.Println(toName)
+			//fmt.Println(err)
+			continue
+		}
 
-        req, _ := http.NewRequest("GET", url, nil)
-        res, err := client.Do(req)
+		for _, cname := range chatData.Chatters.Vips {
+			if cname == name {
+				fmt.Print(toName + " ")
+			}
+		}
 
-        if err != nil {
-            fmt.Println(err)
-            continue 
-        }
+		for _, cname := range chatData.Chatters.Moderators {
+			if cname == name {
+				fmt.Print(toName + " ")
+			}
+		}
 
-        decoder := json.NewDecoder(res.Body)
+		for _, cname := range chatData.Chatters.Viewers {
+			if cname == name {
+				fmt.Print(toName + " ")
+			}
+		}
+	}
+	fmt.Println("")
 
-        var chatData ChatData
-        err = decoder.Decode(&chatData)
-
-        if err != nil {
-            //fmt.Println(toName)
-            //fmt.Println(err)
-            continue 
-        }
-
-        for _, cname := range chatData.Chatters.Vips {
-           if cname == name {
-               fmt.Print(toName + " ")
-           }
-        }
-
-        for _, cname := range chatData.Chatters.Moderators {
-           if cname == name {
-               fmt.Print(toName + " ")
-           }
-        }
-
-        for _, cname := range chatData.Chatters.Viewers {
-           if cname == name {
-               fmt.Print(toName + " ")
-           }
-        }
-    }
-    fmt.Println("")
-
-    tx.Commit()
-    fmt.Println("-----------------------")
+	tx.Commit()
+	fmt.Println("-----------------------")
 }
 
 func parseCommand(str string) {
-    args := strings.Split(str, " ")
-    if len(args) < 2 {
-        fmt.Println("Not enough args")
-    }
+	args := strings.Split(str, " ")
+	if len(args) < 2 {
+		fmt.Println("Not enough args")
+	}
 
-    fmt.Println(len(args))
-    command := args[0]
-    args = args[1:]
+	command := args[0]
+	args = args[1:]
 
-    switch command {
-    case "connect":
-        if len(args) != 1 {
-            fmt.Println("Provide channel name")
-            break
-        }
-        go StartBot(args[0])
-    case "find":
-        if len(args) != 1 {
-            fmt.Println("Provide channel name")
-            break
-        }
+	switch command {
+	case "connect":
+		if len(args) != 1 {
+			fmt.Println("Provide channel name")
+			break
+		}
+		go StartBot(args[0])
+	case "find":
+		if len(args) != 1 {
+			fmt.Println("Provide channel name")
+			break
+		}
 
-        findPerson(args[0])
-    }
+		findPerson(args[0])
+	}
 }
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
-    for {
-        scanner.Scan()
-        args := scanner.Text()
-        parseCommand(args)
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		args := scanner.Text()
+		parseCommand(args)
+	}
 }
