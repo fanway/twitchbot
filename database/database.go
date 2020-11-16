79b148400ffa0160f70ea3594eefa2f2bf8bf3ff
Ivan Avvakumov <avvakumov@protonmail.com>
2020-11-16 09:48:25 +0300

    Fix `database` package to use properly defined paths

--- database/database.go
+++ database/database.go
@@ -2,12 +2,22 @@ package database
 
 import (
 	"database/sql"
+	"path/filepath"
+	"runtime"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
+	name       = "/data.db"
+	settings   = "?_busy_timeout=5000&cache=shared&mode=rwc"
+	params     = basepath + name + settings
+)
+
 func Connect() *sql.DB {
-	db, err := sql.Open("sqlite3", "./data.db?_busy_timeout=5000&cache=shared&mode=rwc")
+	db, err := sql.Open("sqlite3", params)
 	if err != nil {
 		panic(err)
 	}
